468a3b8503aeebc63adb72c6fec0fd25469eb537
samirizwan1999 <muhammadsami6972@gmail.com>
2021-12-15 20:57:03 +0500

    commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// var a=prompt("Enter Your First Name");
+// var b=prompt("Enter Your Last Name");
+// var c=a+b
+// alert("Hello" +c);
+
+
+// var d=prompt("Enter Your Favourite Mobile Model");
+// var char=d.length;
+// console.log(char);
+
+
+// var text="Pakistani";
+// var ind=text.indexOf("n");
+// console.log(ind);
+
+
+// var last="Pakistani";
+// var ind=last.lastIndexOf("i");
+// console.log(ind);   
+
+// var character = "Pakistani";
+// var letter = character.charAt(3);
+// console.log(letter);
+
+
+// var rep = "Hyderabad";
+// var result = rep.replace("Hyder", "Islam");
+// console.log(result);
+
+
+
+
+// var rp = "ALi & Sami are bestfriends.They Play cricket & football together";
+// var rst = rp.replace(/&/g, "and");
+// console.log(rst);
+
+// var txt="Muneeb";
+// var cnvrt=parseInt(txt);
+// console.log(cnvrt);
+// console.log(typeof cnvrt);
+
+
+// var up=prompt("Enter Your Word");
+// alert(up.toUpperCase());
+
+
+
+
+
+
+ 
+
+
+
